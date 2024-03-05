fdfb8f6703fddef4e358c3c03031242d3aebc2c0
Ryuytyuu-Greseven <rukku93223@gmail.com>
2024-03-05 23:54:00 +0530

    Gradle Changes!

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter/material.dart';
 
 import 'src/app.dart';
 import 'src/settings/settings_controller.dart';
